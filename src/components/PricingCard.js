a5125fbcde1abc3ff1a2537ac9756cfad2392fbb
Kriti <kritivij11@gmail.com>
2024-10-17 00:56:36 +0530

    commenting price in cards

--- src/components/PricingCard.js
+++ src/components/PricingCard.js
@@ -8,7 +8,7 @@ const PricingCard = () => {
             <div className="card">
                 <h3>- Basic -</h3>
                 <span className="bar"></span>
-                <p className="btc">$50</p>
+                {/* <p className="btc">$50</p> */}
                 <p>- 3 Days -</p>
                 <p>- 3 Pages -</p>
                 <p>- Responsive Design -</p>
@@ -17,7 +17,7 @@ const PricingCard = () => {
             <div className="card">
                 <h3>- Premium -</h3>
                 <span className="bar"></span>
-                <p className="btc">$500</p>
+                {/* <p className="btc">$500</p> */}
                 <p>- 1-3 Weeks -</p>
                 <p>- Basic Web App -</p>
                 <p>- CRUD operations, simple UI/UX -</p>
@@ -26,7 +26,7 @@ const PricingCard = () => {
             <div className="card">
                 <h3>- Hourly Rate -</h3>
                 <span className="bar"></span>
-                <p className="btc">$20</p>
+                {/* <p className="btc">$20</p> */}
                 <p>- Varies -</p>
                 <p>- Minor Updates -</p>
                 <p>- Bug fixing, Minor features -</p>
